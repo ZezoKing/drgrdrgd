752184988fb08a8f6491d966f161fdc5884d6b11
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-10-26 10:23:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,28 +29,7 @@ const adminprefix = "-"
 client.on('ready', () => {
   console.log(Logged in as ${client.user.tag}!);
 client.user.setGame(اكتب اي شي هنا,"http://twitch.tv/S-F%22)
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log([Start] ${new Date()});
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(Logged in as * [ " ${client.user.username} " ]);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(servers! [ " ${client.guilds.size} " ]);
-  console.log(Users! [ " ${client.users.size} " ]);
-  console.log(channels! [ " ${client.channels.size} " ]);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online ')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
+ 
 
 client.on('message', async msg => { 
 	if (msg.author.bot) return undefined;
