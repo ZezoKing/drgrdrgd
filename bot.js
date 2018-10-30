7894123d52d662b902214eb172b69b4adcff1850
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-10-30 13:08:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,8 @@
-client.on('message', message => {
-    if (message.content.startsWith("+avatar")) {
-if(!message.channel.guild) return;
-        var mentionned = message.mentions.users.first();
-    var client;
-      if(mentionned){
-          var client = mentionned; } else {
-          var client = message.author;
-      }
-        const embed = new Discord.RichEmbed()
-                           .addField('Requested by:', "<@" + message.author.id + ">")
-        .setColor(000000)
-        .setImage(`${client.avatarURL}`)
-      message.channel.sendEmbed(embed);
-    }
+client.on('ready', () => {
+var x = client.channels.get("506763611113390080");
+if (x) x.join();
 });
+
 
 
 
