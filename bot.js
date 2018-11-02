809ef11adb572e618a6c2b5cf9526aee2ec16e25
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-02 10:46:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,36 +6,36 @@ var jimp = require('jimp');
 const moment = require('moment');
 var prefix = "+";
 client.on('message', message => {
-    if (message.content === '.user') {
-        if(!message.channel.guild) return message.reply(`❌ هذا الأمر فقط ل السيرفرات`);
-   
-   message.guild.fetchInvites().then(invs => {
-   let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-   let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-   var moment = require('moment');
-   var men = message.mentions.users.first();
-   var heg;
-   if(men) {
-   heg = men
-   } else {
-   heg = message.author
-   }
-   var mentionned = message.mentions.members.first();
-   var h;
-   if(mentionned) {  /// tm t3del mn Fox
-   h = mentionned
-   } else {
-   h = message.member
-   }
-   moment.locale('ar-TN');
-   var idDark = new  Discord.RichEmbed()
-   
-   .setColor("RANDOM")
-     .setThumbnail(message.author.avatarURL)
-   .addField(' دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('`D/M/YYYY HH:mm`')} **\n** \**${moment(heg.createdTimestamp).fromNow()}**` ,true) 
-   .addField('انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('`D/M/YYYY HH:mm`')} \n \**${moment(h.joinedAt).fromNow()}\**`, true)
-   .addField('عدد الدعوات', `${inviteCount}`, true) /// Dark last
-   .setFooter(message.author.username , message.author.avatarURL)
-   message.channel.sendEmbed(idDark);
-   })}
-   });
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "307630049656438788") return;
+
+if (message.content.startsWith(prefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`Playing: **${argresult}`)
+} 
+
+if (message.content.startsWith(prefix + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/v5bz");
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}`)
+} 
+
+if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+} 
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+}
+});
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
