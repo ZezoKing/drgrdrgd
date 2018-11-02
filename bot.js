c52600cf259be1c11c5127c0b590b7b29856c32b
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-02 22:00:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,13 +45,6 @@ client.on('message', message => {
          message.channel.send({embed:embed});
                         }
                     }); 
-if (message.content === '+help') {
-         let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)    
-      .addField("** مبرمج البوت :wrench: **","**@!      @!  , AhMaD#4900   , @!  , Zezo#7765   **")
-.setColor('RANDOM')
-  message.author.sendEmbed(embed);
-    }
-});
+
 
 client.login(process.env.BOT_TOKEN);
