cf727f09b6918c389e29b4bcb9a5ad48b909d069
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-03 18:27:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,31 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` زيزو يعرف يبرمج  `,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online ')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
-
-client.on('ready', async() => {
-var server = "Id server"; // 506408511240929290
-var channel = "id room";// 506908518591234048
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('ahmad , ahmad , ahmad, ahmad , ahmad , ahmad , ahmad , ahmad , ahmad , ahmad , ahmad , ahmad , ahmad , ahmad , ahmad , ahmad , ahmad,ahmad, ')
-    },305);
-})
 
 
 client.login(process.env.BOT_TOKEN);
