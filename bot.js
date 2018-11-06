e8e804855bca36b6adb47e0d670e8a23552bad7a
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-06 13:09:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 client.on('ready', async() => {
-var server = "اي دي السيرفر"; 
-var channel = "اي دي الروم";
+var server = "509037653040300053"; 
+var channel = "509037699831955456";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**  زيزووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووووو **')
     },305);
