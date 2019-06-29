8d7012ae5dab01c8908d00b95df633267da0b871
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-06-30 01:20:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,33 @@ client.on('ready', async() => {
 var server = "509037653040300053"; 
 var channel = "509037699831955456";
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('** ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما جابت اندومي ماما  ماما جابت اندومي ماما جابت اندومي  **')
+    client.guilds.get(server).channels.get(channel).send('** London Server   **')
     },305);
 }) 
+const fs = require("fs")
+const say = JSON.parse(fs.readFileSync('./say.json', 'utf8'))
 
+client.on("message", message =>
+          {
+      if(!say[message.guild.id]) say[message.guild.id] = {
+        say: 'say'
+        }
+  if(message.content.startsWith(prefix + "say" ) || message.content.startsWith(say[message.guild.id].say)) {
+    var args = message.content.split(" ").slice(1).join(" ")
+    message.channel.send(args)
+  }});
+client.on("message", message => {
+if(message.content.startsWith(prefix + 'set-say')) {
+  var args = message.content.split(" ").slice(1).join(" ")
+    if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
+    if(!say[message.guild.id]) say[message.guild.id] = {
+        say: 'say'
+        }
+        message.channel.send(`**SET THE SAY COMMAND TO ${args}**`), say[message.guild.id].say = args
+        fs.writeFile("./say.json", JSON.stringify(say), (err) => {
+            if (err) console.error(err)
+        })
+}
+})
 
 client.login(process.env.BOT_TOKEN);
